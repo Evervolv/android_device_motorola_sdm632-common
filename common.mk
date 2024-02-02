28ba1a0211b9f1c97454f6594214a2cc3553afbf
William Bellavance Jr <flintman@flintmancomputer.com>
2024-02-02 12:39:50 -0500

    sdm632:  Update to use a different file format when using WITH_GMS
    If WITH_GMS use erofs and if not WITH_GMS use ext4
    
    Change-Id: I477547dfd047cd90b562eb3fb9cce148b2d23818

--- common.mk
+++ common.mk
@@ -38,14 +38,22 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_product=true \
     POSTINSTALL_PATH_product=bin/check_dynamic_partitions \
-    FILESYSTEM_TYPE_product=ext4 \
     POSTINSTALL_OPTIONAL_product=false
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=erofs \
     POSTINSTALL_OPTIONAL_system=true
+
+ifneq ($(WITH_GMS),true)
+AB_OTA_POSTINSTALL_CONFIG += \
+    FILESYSTEM_TYPE_product=ext4 \
+    FILESYSTEM_TYPE_system=ext4
+else
+AB_OTA_POSTINSTALL_CONFIG += \
+    FILESYSTEM_TYPE_product=erofs \
+    FILESYSTEM_TYPE_system=erofs
+endif
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
